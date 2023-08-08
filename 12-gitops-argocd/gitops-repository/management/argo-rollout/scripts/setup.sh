5e9d323bb427f96f9ed7e17834b9426c354bdcec
홍준성 <ghdwnstjd13@gmail.com>
2023-08-08 09:54:59 +0900

    ArgoRollout 구성

--- 12-gitops-argocd/gitops-repository/management/argo-rollout/scripts/setup.sh
+++ 12-gitops-argocd/gitops-repository/management/argo-rollout/scripts/setup.sh
@@ -5,12 +5,12 @@ kubectl create namespace argo-rollouts
 kubectl kustomize ../manifests/ | kubectl apply -f -
 
 # 리눅스에서의 Argo Rollout 플러그인 설치
-#curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
-#chmod +x ./kubectl-argo-rollouts-darwin-amd64
-#sudo mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
+curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-linux-amd64
+chmod +x ./kubectl-argo-rollouts-linux-amd64
+sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
 # MAC OS에서의 Argo Rollout 플러그인 설치
-brew install argoproj/tap/kubectl-argo-rollouts
+#brew install argoproj/tap/kubectl-argo-rollouts
 
 # Argo Rollout Dashboard 기동 (http://localhost:3100)
 kubectl argo rollouts dashboard
